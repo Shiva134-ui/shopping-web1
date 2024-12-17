69da6ee09bff0bff1a7235383af4afb469ffaf07
Shiva134-ui <sivamadesh.134@gmail.com>
2024-12-17 23:16:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('pay-btn').addEventListener('click', () => {
-    const upiLink = 'upi://pay?pa=8928405063@fam&pn=S Madesh&mc=&tid=&tr=&tn=Payment&am=1.00&cu=INR';
+    // UPI Payment Link
+    const upiLink = 'upi://pay?pa=8928405063@fam&pn=S%20Madesh&am=1.00&cu=INR';
     window.location.href = upiLink;
 });
