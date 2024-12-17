7f6acc560c668ce0a6f006c7ca9329d05c8bce64
Shiva134-ui <sivamadesh.134@gmail.com>
2024-12-17 23:02:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('pay-btn').addEventListener('click', () => {
+    const upiLink = 'upi://pay?pa=8928405063@fam&pn=S Madesh&mc=&tid=&tr=&tn=Payment&am=0.00&cu=INR';
+    window.location.href = upiLink;
+});
